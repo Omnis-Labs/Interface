960de4c68a26bee414b259213cb2064943804f5f
Chaitanya <chaitanyagupta172004@gmail.com>
2025-04-14 23:14:33 +0000

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { Hero } from "@/components/hero";
 
@@ -5,12 +6,13 @@ export default function Home() {
   return (
     <div className="relative min-h-screen font-[family-name:var(--font-geist-sans)] text-foreground">
       {/* Background overlay */}
-      <div className="absolute inset-0 bg-[url('/backdrop.jpeg')] bg-cover bg-center bg-no-repeat opacity-16 z-0" />
+      <div className="absolute inset-0 bg-[url('/backdrop.jpeg')] bg-cover bg-center bg-no-repeat bg-fixed opacity-16 z-0" />
 
       {/* Actual content */}
       <div className="relative z-10">
         <Header />
         <Hero />
+        <Footer />
       </div>
     </div>
   );
