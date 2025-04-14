7ab8a12a77d55cf7fc4d1c3228962d6f55278a82
Chaitanya <chaitanyagupta172004@gmail.com>
2025-04-14 23:06:21 +0000

    header and hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import { Header } from "@/components/header";
+import { Hero } from "@/components/hero";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Yzi hackathon demo
+    <div className="relative min-h-screen font-[family-name:var(--font-geist-sans)] text-foreground">
+      {/* Background overlay */}
+      <div className="absolute inset-0 bg-[url('/backdrop.jpeg')] bg-cover bg-center bg-no-repeat opacity-16 z-0" />
+
+      {/* Actual content */}
+      <div className="relative z-10">
+        <Header />
+        <Hero />
+      </div>
     </div>
   );
 }
