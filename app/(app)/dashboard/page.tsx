94830633c91c2666668f88fcf18c6a69544bca0e
Chaitanya <chaitanyagupta172004@gmail.com>
2025-04-24 09:56:11 +0000

    add dashboard cards

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,15 +1,19 @@
+import { PortfolioInfoCard } from "./_components/portfolio-info-card";
 
 export default function Page() {
     return (
         <div className="relative font-[family-name:var(--font-geist-sans)] text-foreground">
-            <div className="pt-4 px-4 mx-auto max-w-6xl space-y-3">
+            <div className="pt-4 px-4 mx-auto max-w-6xl space-y-6">
                 <div className="space-y-1">
                     <p className="text-3xl font-semibold">Dashboard</p>
                     <p className="text-[#363F72]">View and monitor your active portfolio strategies</p>
                 </div>
 
-                <div className="flex items-center justify-center">
-
+                <div className="grid grid-cols-2 lg:grid-cols-4">
+                    <PortfolioInfoCard title={"Total Portfolio Value"} value={"$5,187.50"} status={"+3.75%"} statusColor={"#039855"} footerText={"Initial investment: $5,000.00"} />
+                    <PortfolioInfoCard title={"Current Yield (APY)"} value={"16.5%"} status={"+0.3%"} statusColor={"#039855"} footerText={"vs. 16.2% last week"} />
+                    <PortfolioInfoCard title={"Total Profit"} value={"$187.50"} status={"+12 USDT today"} statusColor={"#039855"} footerText={"Expected monthly: ~$187.00"} />
+                    <PortfolioInfoCard title={"Lock Period"} value={"62 days"} status={"Remaining"} statusColor={"#FB6514"} footerText={"Unlocks on: July 15, 2025"} />
                 </div>
             </div>
         </div>
