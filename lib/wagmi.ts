47f8654dd9ad20e546f655fafe0a6788da9b87d0
Chaitanya <chaitanyagupta172004@gmail.com>
2025-04-25 08:36:48 +0000

    fix wallet adapters

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -3,18 +3,14 @@ import {
     getDefaultConfig,
 } from '@rainbow-me/rainbowkit';
 import {
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
     bsc,
+    mainnet,
 } from 'wagmi/chains';
 
 
 export const config = getDefaultConfig({
     appName: 'Omnis',
     projectId: 'YOUR_PROJECT_ID',
-    chains: [mainnet, polygon, optimism, arbitrum, base, bsc,],
+    chains: [mainnet, bsc,],
     ssr: true, // If your dApp uses server side rendering (SSR)
 });
