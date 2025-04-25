aea3525893234703278a425d9b79fd5d85e7337d
Chaitanya <chaitanyagupta172004@gmail.com>
2025-04-25 09:46:49 +0000

    create aster keys context

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,25 +1,8 @@
-"use client"
 
-import { useWalletAuth } from "@/hooks/useWalletAuth";
-import { useRouter } from "next/navigation";
-import { ReactNode, useEffect } from "react";
-import { StrategyLibrarySkeleton } from "./strategy-library/_components/skeleton";
+import { ReactNode } from "react";
 // import { ChatProvider } from "./ai-portfolio-manager/_contexts/chat";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-    const { isLoading, shouldRedirect } = useWalletAuth()
-    const router = useRouter();
-
-    useEffect(() => {
-        if (shouldRedirect) {
-            router.replace("/sign-in")
-        }
-    }, [shouldRedirect])
-
-    if (isLoading) {
-        return <StrategyLibrarySkeleton />
-    }
-
     return (
         <div className="relative flex flex-col space-y-4 py-2">
             <div className="!mb-6 space-y-4">
--- app/(app)/strategy-library/vaults/[vaultId]/_components/aster-api-input/aster-key-input.tsx
+++ app/(app)/strategy-library/vaults/[vaultId]/_components/aster-api-input/aster-key-input.tsx
@@ -1,17 +1,36 @@
+"use client"
+
 // c53b191df51b2c4696215032c42a620e69a1a1e58031cf90bc2dddc4c07036a2
 // 431d6a452e5e54dedac73cd0fe80510aa22cfcea682c92bbd389a1f75fa2790f
 
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useAsterAPI } from "../../_context/aster-api-context"
 
 export const AsterApiInput = () => {
-    return (
-        <div className="bg-white ">
-            <Label>Aster API Key</Label>
-            <Input />
+    const { asterAPIkey, setAsterAPIkey, asterSecretAPIkey, setAsterSecretAPIkey } = useAsterAPI()
 
-            <Label>Aster API Key</Label>
-            <Input />
+    return (
+        <div className="bg-white p-4 space-y-2 rounded-2xl">
+            <div className="space-y-1">
+                <Label>Aster API Key</Label>
+                <Input
+                    type="text"
+                    value={asterAPIkey}
+                    onChange={(e) => setAsterAPIkey(e.target.value)}
+                    placeholder="Enter API key"
+                />
+            </div>
+
+            <div className="space-y-1">
+                <Label>Aster secret API Key</Label>
+                <Input
+                    type="text"
+                    value={asterSecretAPIkey}
+                    onChange={(e) => setAsterSecretAPIkey(e.target.value)}
+                    placeholder="Enter Secret API key"
+                />
+            </div>
         </div>
     )
 }
